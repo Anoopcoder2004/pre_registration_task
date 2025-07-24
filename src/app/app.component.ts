6733fa6ffb8817cfbd7f84c0680575fd74def468
Anoop <Anooppopziyan@gmail.com>
2025-07-24 17:45:19 +0530

    started add companion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'preregistration_task';
+
+  fileName: string = '';
+  
+  inputGroups: number[] =[];
+  
+  handleFile(event: Event){
+    const input = event.target as HTMLInputElement;
+
+    if(input.files && input.files.length>0){
+      const file = input.files[0];
+      this.fileName = file.name;
+
+    }
+  }
+  onAddClick(){
+    this.inputGroups.push(this.inputGroups.length);
+  }
 }
